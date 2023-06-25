b79286505af74e3979151fd26d396b6fc0e66e94
aszx4510 <aszx4510@gmail.com>
2023-06-25 17:48:51 +0800

    refactor: concatenate filename and default accounting dir

--- data_loader.py
+++ data_loader.py
@@ -1,16 +1,22 @@
-
 import pathlib
 
 import pandas
 
 from file_settings import accounting_dir
 
-file_name = '2023-01-01-2024-01-01.csv'
 
-file_path = accounting_dir / file_name
+def concat_filepath(filename: str, default_dir: pathlib = accounting_dir):
+    return default_dir / filename
 
-print(file_path)
 
-data = pandas.read_csv(file_path)
+def load_money_manager_data(file_name: str):
+    filepath = concat_filepath(file_name, default_dir=accounting_dir)
+    df_data = pandas.read_csv(filepath)
+    print(df_data.shape)
 
-print(data.shape)
+    return df_data
+
+
+if __name__ == '__main__':
+    name = '2023-01-01-2024-01-01.csv'
+    load_money_manager_data(name)
