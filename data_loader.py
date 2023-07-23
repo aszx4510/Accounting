de46f07adc836f8d389aeb54fb78b44794ffb3c0
aszx4510 <aszx4510@gmail.com>
2023-07-23 16:44:36 +0800

    refactor: move ahorro loader into data_loader

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 
 import pandas as pd
@@ -17,6 +18,19 @@ def load_money_manager_data(file_name: str):
     return df_data
 
 
+def load_ahorro_data(file_name: str):
+    filepath = concat_filepath(file_name, default_dir=accounting_dir)
+
+    with open(filepath, 'r', encoding='utf8') as data_input:
+        data = json.load(data_input)
+    return data
+
+
 if __name__ == '__main__':
-    name = '2023-01-01-2024-01-01.csv'
-    load_money_manager_data(name)
+    # name = '2023-01-01-2024-01-01.csv'
+    # load_money_manager_data(name)
+
+    name = 'Ahorro_Backup_20190522144210.json'
+    data_json = load_ahorro_data(name)
+
+    print(data_json)
